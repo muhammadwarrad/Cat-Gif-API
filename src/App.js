a03e07137636a1273104acaa1ea2e2ea798f4de6
muhammadwarrad <moe_warrad@yahoo.com>
2023-01-11 12:33:15 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai"
+  
+
+  
 
 function App() {
+  
+  const [gifs, setGifs] = useState([]);
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    getGif();
+  }, []);
+
+  const getGif = () => {
+    fetch(
+      "https://thingproxy.freeboard.io/fetch/http://api.giphy.com/v1/gifs/search?q=cat&api_key=tuOPOQklxsdCJTh6gEiuj5ZaPKWJ8CQW",
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setGifs(data.data);
+      });
+  };
+// function limit (){
+// if (index === 0) 
+// return (
+// (index + 5)
+// )
+// };
+  function handleClick(){
+    setIndex(prev => {
+      if(prev < 49) {
+        return prev + 1
+      } else {
+        return 0
+      }
+    })
+    console.log(handleClick)
+    
+  };
+  function back(){
+    setIndex(prev => {
+      if (prev > 0){
+        return prev - 1
+      } else {
+        return 49
+      }
+    } )
+    
+  };
+  
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {gifs.length && 
+        <>
+       <h1>CAT Gifs!!!!</h1>
+        <h2>{gifs[index].title}</h2>
+        <h3> NO DATA= Refresh Data</h3>
+       
+          <div className="container">
+         
+          <button className="bob" onClick={back}><AiOutlineArrowLeft/></button>
+          <img src={gifs[index].images.original.url} alt="bruh"/>
+          <button className= "foop" onClick={handleClick}><AiOutlineArrowRight/></button>
+          </div>
+
+        </>
+      }
     </div>
   );
 }
-
 export default App;
